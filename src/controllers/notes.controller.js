f36e0e07382017f00e6c47738c86d965f3fca0ff
Leandro Farias <leandrodavidfarias@gmail.com>
2022-02-12 00:50:59 -0300

    finish

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -9,19 +9,23 @@ notesCtrl.renderNoteFrom = (req, res) => {
 notesCtrl.createNewNote = async (req, res) => {
     const { title, description } = req.body;
     const newNote = new Note({ title: title, description: description })
-    console.log(newNote)
+    newNote.user = req.user.id;
     await newNote.save();
     req.flash('success_msg', 'Note added successfully')
     res.redirect('/notes')
 }
 
 notesCtrl.renderNotes = async (req, res) => {
-    const notes = await Note.find().lean();
+    const notes = await Note.find({ user: req.user.id }).sort({ createdAt: 'desc' }).lean();
     res.render('notes/all-notes', { notes })
 }
 
 notesCtrl.renderEditForm = async (req, res) => {
     const note = await Note.findById(req.params.id).lean();
+    if (note.user != req.user.id) {
+        req.flash('error_msg', 'No authorized')
+        return res.redirect('/notes');
+    }
     console.log(note)
     res.render('notes/edit-note', { note })
 }
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,21 +1,23 @@
 const router = require('express').Router();
 
+const { isAuthenticated } = require('../helpers/auth')
+
 const { renderNoteFrom, createNewNote, renderNotes, renderEditForm, updateNotes, deleteNote } = require('../controllers/notes.controller')
 
 
-router.get('/notes/add', renderNoteFrom)
+router.get('/notes/add', isAuthenticated, renderNoteFrom)
 
-router.post('/notes/new-note', createNewNote)
-
-
-router.get('/notes', renderNotes)
+router.post('/notes/new-note', isAuthenticated, createNewNote)
 
 
-router.get('/notes/edit/:id', renderEditForm)
-
-router.put('/notes/edit-note/:id', updateNotes)
+router.get('/notes', isAuthenticated, renderNotes)
 
 
-router.delete('/notes/delete/:id', deleteNote)
+router.get('/notes/edit/:id', isAuthenticated, renderEditForm)
+
+router.put('/notes/edit-note/:id', isAuthenticated, updateNotes)
+
+
+router.delete('/notes/delete/:id', isAuthenticated, deleteNote)
 
 module.exports = router;
